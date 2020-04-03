661ff639dca79102eabd021923be88c816b5d9d8
John McCourt <1310690+johnsie@users.noreply.github.com>
2020-04-03 11:55:18 +0100

    Add project files.

---
+++ HRUpnp/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HRUpnp
+{
+    partial class frmHRUpnp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHRUpnp));
+            this.btnGo = new System.Windows.Forms.Button();
+            this.prgProgress = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(164, 258);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // prgProgress
+            // 
+            this.prgProgress.ForeColor = System.Drawing.Color.Red;
+            this.prgProgress.Location = new System.Drawing.Point(36, 149);
+            this.prgProgress.Name = "prgProgress";
+            this.prgProgress.Size = new System.Drawing.Size(416, 23);
+            this.prgProgress.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmHRUpnp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(484, 184);
+            this.Controls.Add(this.prgProgress);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmHRUpnp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmHRUpnp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ProgressBar prgProgress;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HRUpnp/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRUpnp
+{
+    public partial class frmHRUpnp : Form
+    {
+        BackgroundWorker bgw = new BackgroundWorker();
+        public frmHRUpnp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+          
+                }//end button
+
+
+
+
+        private void startHover()
+        {
+          string strWorkingDir = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+   
+            startInfo.UseShellExecute = false;
+            startInfo.WorkingDirectory = strWorkingDir;
+            startInfo.RedirectStandardError = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.FileName = "HRace.exe";       
+            process.StartInfo = startInfo;
+
+
+            process.Start();
+            //  string output = process.StandardOutput.ReadToEnd();
+            // MessageBox.Show(output);
+            process.WaitForExit();
+        }
+
+
+
+
+        private void execCmd(string strArgs)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+           startInfo.WindowStyle= ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardError = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.FileName = "miniupnp\\upnpc-shared.exe";
+            startInfo.Arguments = strArgs;
+            startInfo.CreateNoWindow = true;
+            process.StartInfo = startInfo;
+          
+
+            process.Start();
+          //  string output = process.StandardOutput.ReadToEnd();
+           // MessageBox.Show(output);
+            process.WaitForExit();
+        }
+
+        private void frmHRUpnp_Load(object sender, EventArgs e)
+        {
+            bgw.DoWork += new DoWorkEventHandler(bgw_DoWork);
+            bgw.ProgressChanged += new ProgressChangedEventHandler(bgw_ProgressChanged);
+            bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+            bgw.WorkerReportsProgress = true;
+            bgw.RunWorkerAsync();
+
+        }//end load void
+
+
+        void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            int intProgress = 0;
+            //Delete existing rules
+            execCmd("-d 9350 TCP");
+            bgw.ReportProgress(intProgress+=17);
+            execCmd("-d 9351 TCP");
+            bgw.ReportProgress(intProgress += 17);
+            execCmd("-d 9351 UDP");
+            bgw.ReportProgress(intProgress += 17);
+            //Create new rules for this ip
+            execCmd("-a " + localIP + " 9350 9350 TCP");
+            bgw.ReportProgress(intProgress += 17);
+            execCmd("-a " + localIP + " 9351 9351 TCP");
+            bgw.ReportProgress(intProgress += 17);
+            execCmd("-a " + localIP + " 9351 9351 UDP");
+           
+            bgw.ReportProgress(intProgress+=15);
+        }
+
+
+        void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            prgProgress.Value = e.ProgressPercentage;
+  
+        }
+
+        void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //do the code when bgv completes its work
+            startHover();
+        }
+
+    }//end class
+}//end namespace
